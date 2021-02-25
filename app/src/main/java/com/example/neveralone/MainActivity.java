85d86fe6227ef3a19bb5fbb911d7305e403e15ee
fahim <fahimgha10@gmail.com>
2021-02-25 13:25:54 +0100

    Amélioration du login et du signUp

--- app/src/main/java/com/example/neveralone/MainActivity.java
+++ app/src/main/java/com/example/neveralone/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.neveralone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void goToLogin(View V){
+        Intent myIntent  =  new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(myIntent);
+    }
+    public void goToRegister(View V){
+        Intent myIntent  =  new Intent(MainActivity.this,RegisterActivity.class);
+        startActivity(myIntent);
     }
 }
--- app/src/main/java/com/example/neveralone/NameActivity.java
+++ app/src/main/java/com/example/neveralone/NameActivity.java
@@ -3,19 +3,93 @@ package com.example.neveralone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Random;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class NameActivity extends AppCompatActivity {
-    String email,password;
+    String email, password;
+    EditText e_PersonName;
+    CircleImageView circleImageView;
+
+    Uri resultUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name);
+        e_PersonName = (EditText) findViewById(R.id.editTextTextPersonName);
+        circleImageView = (CircleImageView) findViewById(R.id.circleImageView);
         Intent myIntent = getIntent();
-        if(myIntent!=null)
-        {
-            email =myIntent.getStringExtra("email");
-            password =myIntent.getStringExtra("mot de passe");
+        if (myIntent != null) {
+            email = myIntent.getStringExtra("email");
+            password = myIntent.getStringExtra("mot de passe");
+        }
+    }
+
+    public void generateCode(View V) {
+        Date myDate = new Date();
+        SimpleDateFormat format1 = new SimpleDateFormat("aaaa-MM-dd HH:mm:ss a", Locale.getDefault());
+        String date = format1.format(myDate);
+        Random r = new Random();
+
+        int n = 10000 + r.nextInt(900000);
+        String code = String.valueOf(n);
+
+        if (resultUri != null) {
+            Intent myIntent = new Intent(NameActivity.this, InviteCodeActivity.class);
+            myIntent.putExtra("name", e_PersonName.getText().toString());
+            myIntent.putExtra("email", email);
+            myIntent.putExtra("password", password);
+            myIntent.putExtra("date", myDate);
+            myIntent.putExtra("isSharing", false);
+            myIntent.putExtra("code", code);
+            myIntent.putExtra("ImageUri", resultUri);
+
+
+            startActivity(myIntent);
+
+        } else {
+            Toast.makeText(getApplicationContext(), "Choisiez une Image s'il vous plait", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void selectImage(View V) {
+        Intent i = new Intent();
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        i.setType("images/*");
+        startActivityForResult(i, 12);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 12 && resultCode == RESULT_OK && data != null) {
+            // start picker to get image for cropping and then use the image in cropping activity
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(1,1)
+                    .start(this);
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                resultUri = result.getUri();
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
         }
     }
 }
